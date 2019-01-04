70411203de7b94a7998d4db43c664347753139f0
Pantelis Papageorgiou <40605639+PanPapag@users.noreply.github.com>
2019-01-04 12:20:37 +0200

    Add files via upload

--- PlantImplementation.cpp
+++ PlantImplementation.cpp
@@ -2,73 +2,114 @@
 #include <cstring>
 #include "PlantInterface.h"
 
-using namespace std;
-
 bool RandomResult(int factor){
-  int res = rand() % 100;
+  int res = rand() % 101;
   if (res > factor) return true;
   else return false;
 }
 
-/*Struct Plant functions */
+/* -------------------------------------- class Plant Implementation ------------------------------------- */
 
-Plant::Plant(char* nam, int xx, int yy, char tok, int breeding, int illness, int life_var, int life_fact)
-  :token(tok), breeding_prob(breeding), illness_prob(illness), life(life_var), life_factor(life_fact){
-    name = new char[strlen(nam) + 1];
-    strcpy(name, nam);
-    coordinates = new point(xx,yy);
-    cout << "I just constructed a " << name << endl;
-  }
-
-Plant::~Plant(){
-  cout << "Deleting a " << name << endl;
-  delete [] name;
-  delete coordinates;
+Plant::Plant(string name, int x, int y, char token, int breeding, int illness, int life_var, int life_fact)
+  : name(name), coordinate_x(x), coordinate_y(y), token(token),
+    breeding_prob(breeding), illness_prob(illness), life(life_var), life_factor(life_fact) {
+      //cout << "I just constructed a plant" << endl;
 }
-bool Plant::IsDead(void) { return (life == 0); }
+
+Plant::~Plant() {
+  //cout << "Destructed a plant" << endl;
+}
+
+bool Plant::Reproduced(void) { return (RandomResult(1 - breeding_prob)); }
+
+bool Plant::IsDead(void) { return (life <= 0); }
 
 int Plant::GetLife(void) { return life; }
 
-/*Seedless Plant functions */
+int Plant::GetCoordinateX(void) { return coordinate_x; }
 
-Seedless::Seedless(char* nam, int xx, int yy, char tok, int breeding, int illness, int life_var, int life_fact)
-  : Plant(nam, xx, yy, tok, breeding, illness, life_var, life_fact){}
+int Plant::GetCoordinateY(void) { return coordinate_y; }
 
-void Seedless::LooseLife(){ life -= life_factor; }
+int Plant::GetBreedingProb(void) { return breeding_prob; }
 
-void Seedless::EatenByAnimal(int quantity){
-  if(quantity < life) life -= quantity;
-  else life = 0;
+int Plant::GetIllnessProb(void) { return illness_prob; }
+
+int Plant::GetLifeFactor(void) { return life_factor; }
+
+char Plant::GetToken(void) { return token; }
+
+string Plant::GetName(void) { return name; }
+
+
+/* -------------------------------------- Seedless Implementation ------------------------------------- */
+
+Seedless::Seedless(string name, int x, int y, char token, int breeding, int illness, int life_var, int life_fact)
+  : Plant(name, x, y, token, breeding, illness, life_var, life_fact) {
+    //cout << "I just constructed a seedless plant" << endl;
 }
 
-void Seedless::Grow(void){
-  int action = RandomResult(illness_prob);
-  if (action) life += life_factor;
-  else life -= life_factor;
+Seedless::~Seedless() {
+  //cout << "Destructed a seedless plant" << endl;
 }
 
-bool Seedless::Reproduced(void){ //otan katalaveis ti kanei tha entypwsiasteis
-  return (RandomResult(1 - breeding_prob));
+void Seedless::LoseLife(void) { life -= life_factor; }
+
+void Seedless::EatenByAnimal(int quantity) {
+  if(quantity < life) {
+    life -= quantity;
+  } else {
+    life = 0;
+  }
 }
+
+void Seedless::Grow(void) {
+  bool action = RandomResult(illness_prob);
+  if (action) {
+    life += life_factor;
+  } else {
+    life -= life_factor;
+  }
+}
+
+Grass::Grass(string name, int x, int y, char token, int breeding, int illness, int life_var, int life_fact)
+  : Seedless(name, x, y, token, breeding, illness, life_var, life_fact) {
+    //cout << "I just constructed a grass plant" << endl;
+}
+
+Grass::~Grass() {
+  //cout << "Destructed a grass plant" << endl;
+}
+
+Algae::Algae(string name, int x, int y, char token, int breeding, int illness, int life_var, int life_fact)
+  : Seedless(name, x, y, token, breeding, illness, life_var, life_fact) {
+    //cout << "I just constructed an Algae plant" << endl;
+}
+
+Algae::~Algae() {
+  //cout << "Destructed an Algae plant" << endl;
+}
+
 /*Leaves functions */
 
-Leaves::Leaves(int n_of_leaves): quantity(n_of_leaves), size(1) {}
+Leaves::Leaves(int no_of_leaves): quantity(no_of_leaves), size(1) {}
 
-int Leaves::GetNumber(void){ return quantity; }
+int Leaves::GetNumber(void) { return quantity; }
 
-void Leaves::IncreaseSize(void){ size++; }
+void Leaves::IncreaseSize(void) { size++; }
 
-void Leaves::DecreaseSize(void){ size--; }
+void Leaves::DecreaseSize(void) { size--; }
 
-void Leaves::IncreaseNumber(int n){ this->quantity += n;}
+void Leaves::IncreaseNumber(int n) { quantity += n; }
 
-void Leaves::DecreaseNumber(int n){ this->quantity -= n;}
+void Leaves::DecreaseNumber(int n) { quantity -= n; }
 
 
 /*Seeded Plant functions */
 
-Seeded::Seeded(char* nam, int xx, int yy, char tok, int breeding, int illness, int life_var, int life_fact, int fol, int seed)
-  :Plant(nam, xx, yy, tok, breeding, illness, life_var, life_fact), foliage(fol), seeds(seed){}
+Seeded::Seeded(string name, int x, int y, char token, int breeding, int illness, int life_var, int life_fact, int fol, int seed)
+  :Plant(name, x, y, token, breeding, illness, life_var, life_fact), foliage(fol), seeds(seed) {
+    //cout << "I just constructed a seeded plant" << endl;
+}
 
 void Seeded::EatenByAnimal(int quantity){
   if (quantity < seeds ) seeds -= quantity;
@@ -85,21 +126,22 @@ void Seeded::EatenByAnimal(int quantity){
 }
 
 void Seeded::Grow(void){
-  int action = RandomResult(illness_prob);
+  bool action = RandomResult(illness_prob);
+
   if (action) {
     foliage.IncreaseNumber(life_factor);
     seeds += 2 * life_factor;
-  }
-  else{
+  } else {
     foliage.DecreaseNumber(life_factor);
     seeds -= 2 * life_factor;
   }
-  if( foliage.GetNumber() % life_factor == 0 ){
-    if (action) foliage.IncreaseSize();
-    else  foliage.DecreaseSize();
-  }
-}
 
-bool Seeded::Reproduced(void){
-  return (RandomResult(1 - breeding_prob));
+  if(foliage.GetNumber() % life_factor == 0) {
+    if (action) {
+      foliage.IncreaseSize();
+    } else {
+      foliage.DecreaseSize();
+    }
+  }
+
 }
